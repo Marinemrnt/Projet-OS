db144bb4cc2569897e88867967c42720ed39670a
dodolerigolo <116345694+dodolerigolo@users.noreply.github.com>
2022-11-03 11:41:27 +0100

    Update pere.c

--- script/pere.c
+++ script/pere.c
@@ -8,28 +8,18 @@
 #include <fcntl.h>
 
 
-
-
 int main(void)
 {
 
-while(1)
-{
-        int pid, choix;
-        FILE *fp;
-        int stat, ch2;
-        int i = 0;
-        char ch;
-        char * argv[2];
+        while(1)
+        {
+                int pid, choix, status;
+                FILE *fp;
+                int stat, ch2;
+                int i = 0;
+                char ch;
+                char * argv[2];
 
-        pid=fork();
-
-        if(pid == 0){
-                argv[0]="F";
-                argv[1]=NULL;
-			 execv("F",argv);
-                }
-        else{
                 printf("\nMenu\n");
                 printf("0.Quitter\n");
                 printf("1.Allumer/Eteindre LED\n");
@@ -47,19 +37,36 @@ while(1)
                                 break;
 
                         case 1 :
-                                printf("PARENT : press ON/OFF pour allumer/eteindre la LED\n");
+				pid=fork();
+
+                                if(pid == 0){
+                                      argv[0]="F";
+                                      argv[1]=NULL;
+                                      execv("F",argv);
+                                      }
+
+                                else{
+
+                                printf("PARENT : press on/off pour allumer/eteindre la LED\n");
                                 scanf("%s",&ch);
 
-                                if(strcmp(&ch, "ON") == 0)
-                                        {kill(pid,SIGUSR1);}
+                                if(strcmp(&ch,"on") == 0)
+                                        {kill(pid,SIGUSR1);
+                                        wait(&status);
+                                        printf("\nfils on exit status : %d\n",status);
+                                }
 
- 						  if(strcmp(&ch, "OFF") == 0)
-                                        {kill(pid,SIGUSR2);}
+
+                                if(strcmp(&ch,"off") == 0)
+                                        {kill(pid,SIGUSR2);
+                                        wait(&status);
+                                        printf("\nfils off exit status : %d\n",status);
+                                        }
+                                }
 
                                 break;
-
-
-                        case 2 :
+				case 2 :
+				
                                 printf("PARENT : Combien de fois souhaitez-vous faire clignoter la LED ?\n");
                                 scanf("%d",&ch2);
 
@@ -79,19 +86,19 @@ while(1)
                                                 /* Handle error */;
 
                                         sleep(1);
-								i++;
-                                }
-                                break;
+                                        stat = pclose(fp);
+                                        i++;
 
-                        default :
+                                }
+
+                                break;
+				 default :
                                 exit(1);
                                 break;
 
                 }
 
-                stat = pclose(fp);
-                wait(NULL);
                 printf("\nProgramme termine\n");
+
         }
-}
 }
