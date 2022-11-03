f77791ddca3a05f5d024b5c58c5b08900ac20ddf
LucasBrutus <116345922+LucasBrutus@users.noreply.github.com>
2022-11-03 09:37:03 +0100

    Create fils.c

---
+++ script/fils.c
@@ -0,0 +1,72 @@
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <signal.h>
+#include <fcntl.h>
+#include <string.h>
+
+#define LINE_BUFSIZE 128
+
+
+void ON (){
+
+        FILE *fp;
+        int status;
+        char line[LINE_BUFSIZE];
+        int linenr;
+
+        //execution script bash ON//
+
+        fp = popen("./script_LEDon.sh", "r");
+        if (fp == NULL)
+        /* Handle error */;
+
+        //Lecture reponse ESP32//
+
+        linenr = 1;
+        while (fgets(line, LINE_BUFSIZE, fp) != NULL) {
+	   printf("Reponse %d: %s", linenr, line);
+        ++linenr;}
+
+        status = pclose(fp);
+        printf("Programme termine");
+        exit(3);
+}
+
+void OFF (){
+
+        FILE *fp;
+        int status;
+        char line[LINE_BUFSIZE];
+        int linenr;
+
+        //execution script bash ON//
+
+        fp = popen("./script_LEDoff.sh", "r");
+        if (fp == NULL)
+                /* Handle error */;
+
+        //Lecture reponse ESP32//
+
+        linenr = 1;
+        while (fgets(line, LINE_BUFSIZE, fp) != NULL) {
+		printf("Reponse %d: %s", linenr, line);                                                                                     ++linenr;}
+
+        status = pclose(fp);
+        printf("Programme termine");
+        exit(3);
+
+}
+
+int main(void)
+{
+        signal(SIGUSR1, ON);
+        signal(SIGUSR2, OFF);
+        for(;;)
+        {
+                sleep(1);
+        }
+
+}
